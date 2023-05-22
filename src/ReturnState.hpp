552d16d27a2fc040c425c6798b4ed5e513d3ec86
kimchijinju <hanbimkim@gmail.com>
2023-05-22 15:16:35 +0900

    feat: define ReturnState

---
+++ src/ReturnState.hpp
@@ -0,0 +1,12 @@
+// Copyright 2023 ean, hanbkim, jiyunpar
+
+#ifndef SRC_STATE_HPP
+#define SRC_STATE_HPP
+
+enum ReturnState {
+  SUCCESS,
+  FAIL,
+  CONNECTION_CLOSE,
+};
+
+#endif // SRC_STATE_HPP
