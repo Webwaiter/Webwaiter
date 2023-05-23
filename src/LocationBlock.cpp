675ddc703f3364e069fef55ea876e4051456c504
flowjiyun <gyp2795@gmail.com>
2023-05-23 23:39:15 +0900

    feat: add location block parsing logic

--- src/LocationBlock.cpp
+++ src/LocationBlock.cpp
@@ -1,11 +1,65 @@
 // Copyright 2023 ean, hanbkim, jiyunpar
 
 #include "src/LocationBlock.hpp"
+#include "src/ReturnState.hpp"
+#include "src/utils.hpp"
 
-LocationBlock::LocationBlock(std::fstream &file) {
-
+LocationBlock::LocationBlock(std::fstream &file, std::string url) :url_(url) {
+  try {
+    parseLocationBlock(file);
+  } catch (int) {
+    throw FAIL;
+  }
 }
 
-void LocationBlock::parseLocationBlock(void) {
-	
+void LocationBlock::parseLocationBlock(std::fstream &file) {
+  int error_flag = 0;
+  std::stack<std::string> brace;
+  brace.push("{");
+  while (!brace.empty()) {
+    std::string line;
+    std::getline(file, line);
+    std::string tmp = skipCharset(line, " \t");
+    std::vector<std::string> tmp_vec = split(tmp, " ");
+    // check error_case 
+    if (tmp == "}") {
+      if (brace.top() == "{") {
+        brace.pop();
+      }
+    } else {
+      if (tmp_vec[0] == "root_dir") {
+        root_dir_ = tmp_vec[1];
+        error_flag |= (1 << 0);
+      } else if (tmp_vec[0] == "allowed_method") {
+        for (size_t i = 1; i < tmp_vec.size(); ++i) {
+          allowed_method_.insert(tmp_vec[i]);
+        }
+        error_flag |= (1 << 1);
+      } else if (tmp_vec[0] == "directory_listing") {
+        if (tmp_vec[1] == "on") {
+          directory_listing_ = true;
+          error_flag |= (1 << 2);
+        } else if (tmp_vec[1] == "off") {
+          directory_listing_ = false;
+          error_flag |= (1 << 2);
+        } else {
+          throw FAIL;
+        }
+      } else if (tmp_vec[0] == "index") {
+        index_ = tmp_vec[1];
+        error_flag |= (1 << 3);
+      } else if (tmp_vec[0] == "cgi_extension") {
+        cgi_extension_ = tmp_vec[1];
+        error_flag |= (1 << 4);
+      } else if (tmp_vec[0] == "cgi_path") {
+        cgi_path_ = tmp_vec[1];
+        error_flag |=  (1 << 5);
+      } else if (tmp_vec[0] == "redirection") {
+        redirection_ = tmp_vec[1];
+        error_flag |=  (1 << 6);
+      }
+  }
+  if (error_flag != 31 && error_flag != 63) {
+    throw FAIL;	
+  }
 }
--- src/LocationBlock.hpp
+++ src/LocationBlock.hpp
@@ -6,22 +6,23 @@
 #include <fstream>
 #include <string>
 #include <vector>
+#include <stack>
 #include <set>
 
 class LocationBlock {
  public:
-  explicit LocationBlock(std::fstream &file);
+  explicit LocationBlock(std::fstream &file, std::string url);
 
  private: 
-  void parseLocationBlock(void);
+  void parseLocationBlock(std::fstream &file);
   std::string url_;
   std::string root_dir_;
   std::set<std::string> allowed_method_;
-  std::string redirection_;
   bool directory_listing_;
   std::string index_;
   std::string cgi_extension_;
   std::string cgi_path_;
+  std::string redirection_;
 };
 
 #endif  // SRC_LOCATIONBLOCK_HPP_
