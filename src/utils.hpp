72653cfc49bdcc766609b22925e168187d2f8d0b
flowjiyun <gyp2795@gmail.com>
2023-05-25 15:15:18 +0900

    fix: restore merge error on utils.hpp file

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,2 +1,19 @@
+// Copyright 2023 ean, hanbkim, jiyunpar
+
+#ifndef SRC_UTILS_HPP_
+#define SRC_UTILS_HPP_
+
+#include <string>
+#include <vector>
+
+enum ReturnState {
+  SUCCESS,
+  FAIL,
+  AGAIN,
+  CONNECTION_CLOSE
+};
+
+std::vector<std::string> split(std::string input, std::string delimiter);
+std::string skipCharset(std::string input, std::string charset);
 
 #endif //SRC_UTILS_HPP_
