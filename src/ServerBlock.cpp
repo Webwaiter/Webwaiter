45eac8e5683c892b89a8575bf4984e869e7b9ec0
flowjiyun <gyp2795@gmail.com>
2023-05-28 21:11:22 +0900

    feat: add checkSemantics() function for server block

--- src/ServerBlock.cpp
+++ src/ServerBlock.cpp
@@ -26,14 +26,59 @@ static void checkBracePair(std::vector<std::string> &tmp_vec, std::stack<std::st
     }
   }
 }
-static void checkDefaultErrorPage(std::map<std::string, std::string> &default_error_page) {
+static void checkDefaultErrorPage(std::map<std::string, std::string> default_error_page) {
+  if (access(default_error_page["200"].c_str(), R_OK | F_OK) == -1) {
+    throw FAIL; 
+  }
+  if (access(default_error_page["300"].c_str(), R_OK | F_OK) == -1) {
+    throw FAIL; 
+  }
   if (access(default_error_page["400"].c_str(), R_OK | F_OK) == -1) {
-    
+    throw FAIL; 
+  }
+  if (access(default_error_page["500"].c_str(), R_OK | F_OK) == -1) {
+    throw FAIL; 
   }
 }
-void ServerBlock::checkSemantics(void) const {
 
+static void checkClientBodySize(int client_body_size) {
+  if (client_body_size <= 0 || client_body_size > 10485760) {
+    throw FAIL;
+  }
 }
+
+static void checkServerIP(std::string server_ip) {
+  std::vector<std::string> ip;
+
+  ip = split(server_ip, ".");
+  if (ip.size() != 4) {
+    throw FAIL;
+  }
+  for (int i = 0; i < ip.size(); ++i) {
+    for (int j = 0; j < ip[i].size(); ++j) {
+      if (!isdigit(ip[i][j])) {
+        throw FAIL;
+      }
+    }
+    double token = strtod(ip[i].c_str(), NULL);
+    if (!(token >= 0 && token <= 255)) {
+      throw FAIL;
+    }
+  }
+}
+static void checkServerPort(int server_port) {
+  if (!(server_port >= 0 && server_port <= 65535)) {
+    throw FAIL;
+  }
+}
+
+void ServerBlock::checkSemantics(void) const {
+  checkDefaultErrorPage(default_error_pages_);
+  checkClientBodySize(client_body_size_);
+  checkServerIP(server_ip_);
+  checkServerPort(server_port_);
+}
+
 void ServerBlock::parseServerBlock(std::fstream &file) {
   int error_flag = 0;
   std::stack<std::string> brace;
@@ -53,27 +98,33 @@ void ServerBlock::parseServerBlock(std::fstream &file) {
       location_blocks_.push_back(LocationBlock(file, tmp_vec[1]));
       brace.pop();
       error_flag |= (1 << 0);
+    } else if (tmp_vec[0] == "default_error_page_200" && tmp_vec.size() == 2) {
+      default_error_pages_["200"] = tmp_vec[1];
+      error_flag |= (1 << 1);
+    } else if (tmp_vec[0] == "default_error_page_300" && tmp_vec.size() == 2) {
+      default_error_pages_["300"] = tmp_vec[1];
+      error_flag |= (1 << 2);
     } else if (tmp_vec[0] == "default_error_page_400" && tmp_vec.size() == 2) {
       default_error_pages_["400"] = tmp_vec[1];
-      error_flag |= (1 << 1);
+      error_flag |= (1 << 3);
     } else if (tmp_vec[0] == "default_error_page_500" && tmp_vec.size() == 2) {
       default_error_pages_["500"] = tmp_vec[1];
-      error_flag |= (1 << 2);
+      error_flag |= (1 << 4);
     } else if (tmp_vec[0] == "client_body_size" && tmp_vec.size() == 2) {
       client_body_size_ = atoi(tmp_vec[1].c_str());
-      error_flag |= (1 << 3);
+      error_flag |= (1 << 5);
     } else if (tmp_vec[0] == "listen" && tmp_vec.size() == 2) {
       server_ip_ = tmp_vec[1];
-      error_flag |= (1 << 4);
+      error_flag |= (1 << 6);
     } else if (tmp_vec[0] == "port" && tmp_vec.size() == 2) {
       server_port_ = atoi(tmp_vec[1].c_str());
-      error_flag |= (1 << 5);
+      error_flag |= (1 << 7);
     } else if (tmp_vec[0] == "server_name" && tmp_vec.size() == 2) {
       server_name_ = tmp_vec[1];
-      error_flag |=  (1 << 6);
+      error_flag |=  (1 << 8);
     }
   }
-  if (error_flag != 63 && error_flag != 127 && !brace.empty()) {
+  if (error_flag != 127 && error_flag != 255 && !brace.empty()) {
     throw FAIL;
   }
 }
--- src/ServerBlock.hpp
+++ src/ServerBlock.hpp
@@ -11,6 +11,7 @@
 #include <map>
 #include <stack>
 #include <cstdlib>
+#include <cctype>
 
 #include "src/LocationBlock.hpp"
 
