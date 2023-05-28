f620efa5ecd17dd550225b21b42e00b12f00e6fc
flowjiyun <gyp2795@gmail.com>
2023-05-28 22:02:06 +0900

    feat: add parsing logic for mime types, status message and check semantics for config block

--- src/Config.cpp
+++ src/Config.cpp
@@ -7,6 +7,7 @@
 Config::Config(const char *file_path) {
   try {
     parseConfigFile(file_path);
+    checkSemantics();
   }
   catch(int) {
     throw FAIL;
@@ -25,6 +26,30 @@ static void checkBracePair(std::vector<std::string> &tmp_vec, std::stack<std::st
       }
     }
   }
+}
+
+static void checkHTTPVersion(std::string http_version) {
+  if (http_version != "1.1") {
+    throw FAIL;
+  }
+}
+
+static void checkStatusPath(std::string status_path) {
+  if (access(status_path.c_str(), R_OK | F_OK) == -1) {
+    throw FAIL;
+  }
+}
+
+static void checkMIMEPath(std::string mime_path) {
+  if (access(mime_path.c_str(), R_OK | F_OK) == -1) {
+    throw FAIL;
+  }
+}
+
+void Config::checkSemantics(void) const {
+  checkHTTPVersion(http_version_);
+  checkStatusPath(status_path_);
+  checkMIMEPath(mime_path_);
 }
 
 void Config::parseConfigFile(const char *file_path) {
@@ -53,15 +78,67 @@ void Config::parseConfigFile(const char *file_path) {
       brace.pop();
       error_flag |= (1 << 0);
     } else if (tmp_vec[0] == "program_name" && tmp_vec.size() == 2) {
-        server_program_name_ = tmp_vec[1];
-        error_flag |= (1 << 1);
+      server_program_name_ = tmp_vec[1];
+      error_flag |= (1 << 1);
     } else if (tmp_vec[0] == "http_version" && tmp_vec.size() == 2) {
-        http_version_ = tmp_vec[1];
-        error_flag |= (1 << 2);
+      http_version_ = tmp_vec[1];
+      error_flag |= (1 << 2);
+    } else if (tmp_vec[0] == "status_path" && tmp_vec.size() == 2) {
+      status_path_ = tmp_vec[1];
+      parseStatusFile(status_path_.c_str());
+      error_flag |= (1 << 3);
+    } else if (tmp_vec[0] == "mime_path" && tmp_vec.size() == 2) {
+      mime_path_ = tmp_vec[1];
+      parseMIMEFile(mime_path_.c_str());
+      error_flag |= (1 << 4);
     }
   }
-  if (error_flag != 7 && !brace.empty()) {
+  if (error_flag != 31 && !brace.empty()) {
     throw FAIL;
+  }
+}
+
+void Config::parseStatusFile(const char *file_path) {
+  std::fstream file;
+
+  file.open(file_path, std::fstream::in);
+  if (!file.is_open()) {
+    throw FAIL;
+  }
+  while (!file.eof()) {
+    std::string line;
+    std::getline(file, line);
+    std::string tmp = skipCharset(line, " \t");
+    std::vector<std::string> tmp_vec = split(line, ":");
+    if (tmp == "") {
+      continue;
+    }
+    if (tmp_vec.size() == 2) {
+      status_messages_[atoi(tmp_vec[0].c_str())] = tmp_vec[1];
+    }
+  }
+}
+
+void Config::parseMIMEFile(const char *file_path) {
+  std::fstream file;
+
+  file.open(file_path, std::fstream::in);
+  if (!file.is_open()) {
+    throw FAIL;
+  }
+  while (!file.eof()) {
+    std::string line;
+    std::getline(file, line);
+    std::string tmp = skipCharset(line, " \t");
+    std::vector<std::string> tmp_vec = split(line, " \t");
+    if (tmp == "") {
+      continue;
+    }
+    if (tmp_vec.size() != 1) {
+      for (int i = 1; i < tmp_vec.size(); ++i) {
+        mime_types_[tmp_vec[i]] = tmp_vec[0];
+      }
+    }
   }
 }
 
--- src/Config.hpp
+++ src/Config.hpp
@@ -4,6 +4,7 @@
 #define SRC_CONFIG_HPP_
 
 #include <netinet/in.h>
+#include <unistd.h>
 
 #include <iostream>
 #include <fstream>
@@ -23,8 +24,15 @@ class Config {
 
  private:
   void parseConfigFile(const char *file_path);
+  void parseStatusFile(const char *file_path);
+  void parseMIMEFile(const char *file_path);
+  void checkSemantics(void) const;
   std::string server_program_name_;
   std::string http_version_;
+  std::string status_path_;
+  std::string mime_path_;
+  std::map<int, std::string> status_messages_;
+  std::map<std::string, std::string> mime_types_;
   std::vector<ServerBlock> server_blocks_; 
 };
 
