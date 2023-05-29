9d0a910e913f7bff41bbdff4a8375590243d9fcf
flowjiyun <gyp2795@gmail.com>
2023-05-29 17:41:09 +0900

    fix: add member variables to Connection Class

--- src/Connection.hpp
+++ src/Connection.hpp
@@ -3,22 +3,27 @@
 #ifndef SRC_CONNECTION_HPP_
 #define SRC_CONNECTION_HPP_
 
+#include <netinet/in.h>
 #include <queue>
 #include <string>
 
+#include "src/Config.hpp"
+#include "src/ServerBlock.hpp"
+#include "src/LocationBlock.hpp"
 #include "src/Kqueue.hpp"
 #include "src/RequestMessage.hpp"
 #include "src/ResponseMessage.hpp"
-#include "src/ReturnState.hpp"
+#include "src/utils.hpp"
 
 class Connection {
  public:
-  Connection(int connection_socket, const Kqueue& kqueue);
+  Connection(int connection_socket, const Kqueue& kqueue, const Config& config);
   int getConnectionSocket() const;
   char *getReadBuffer();
   ReturnState work();
   bool checkReadSuccess();
   bool checkTimeOut();
+  void setConfigInfo();
   void parsingRequestMessage();
   void executeCGIProcess();
   void openStaticPage();
@@ -42,7 +47,11 @@ class Connection {
   RequestMessage request_message_;
   ResponseMessage response_message_;
   State state_;
-  Kqueue kqueue_;
+  const Kqueue &kqueue_;
+  const Config &config_;
+  ServerBlock *cur_server_;
+  LocationBlock *cur_location_;
+  struct sockaddr_in client_addr_;
   int response_status_code_;
 };
 
