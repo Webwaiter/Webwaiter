9e07e7e61eed892e67e70bd7d970ffb310fbed38
flowjiyun <gyp2795@gmail.com>
2023-06-05 20:17:24 +0900

    docs: test file for cgi test

---
+++ docs/html/index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+print("Content-Type: text/html")
+print
+print("<html>")
+print("<head>")
+print("<title>CGI Example</title>")
+print("</head>")
+print("<body>")
+print("<h1>Welcome to the CGI Example</h1>")
+
+print("<p>Please enter your name.</p>")
+
+print("</body>")
+print("</html>\x1A")
